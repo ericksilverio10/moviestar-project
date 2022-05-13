712ff61a8e5bb1ef6b669c39c31a85956be611b0
Erick <erick1junior@hotmail.com>
2022-05-13 20:02:44 -0300

    Criando página de autenticação

--- auth.php
+++ auth.php
@@ -3,7 +3,31 @@
 ?>
 
 <div id="main-container" class="container-fluid">
-        <h1>AUTENTICAÇÃO</h1>
+    <div class="col-md-12">
+        <div class="row" id="auth-row">
+            <div class="col-md-4" id="login-container">
+                <h2>Entrar</h2>
+                <form action="" method="POST">
+                <input type="hidden" name="type" value="login">
+                    <div class="form-group">
+                        <label for="email">E-mail:</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="seuemail@email.com">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Senha:</label>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Digite a sua senha">
+                    </div>
+                    <input type="submit" class="btn card-btn" value="Entrar">
+                </form>
+            </div>
+            <div class="col-md-4" id="register-container">
+                <h2>Criar conta</h2>
+                <form action="" method="POST">
+                    <input type="hidden" name="type" value="register">
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 
 <?php
